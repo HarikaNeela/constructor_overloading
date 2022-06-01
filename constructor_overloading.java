a9f717451bc864d9a9c4e623c35524b2c9b95a57
Harika Neela <97505929+HarikaNeela@users.noreply.github.com>
2022-06-01 22:43:45 +0530

    Add files via upload

---
+++ constructor_overloading.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class rect
+{
+    int a;
+    int b;
+    rect()
+    {
+        System.out.println(""+30);
+    }
+    rect(int p,int q)
+    {
+        a=p;
+        b=q;
+        System.out.println(""+(a+b));
+    }
+}
+class result
+{
+    public static void main (String[] args) {
+       
+        rect r1=new rect();
+        
+    }
+}
